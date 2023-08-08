755e021e5b2173a71d3dfd36444959cb16b24862
JulianSweetwood <juliansussholz17@gmail.com>
2023-08-08 17:29:47 +0200

    Header/Footer basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
-    <h1>
-      Welcome to my shop biatchez
-    </h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+};
 
-export default App
+export default App;
