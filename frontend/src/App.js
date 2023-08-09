9658f4ca18ae35813c6558a4c33efe8bef3fe56e
JulianSweetwood <juliansussholz17@gmail.com>
2023-08-09 16:13:30 +0200

    ProductScreenand Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+
 
 const App = () => {
   return (
@@ -9,7 +11,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
