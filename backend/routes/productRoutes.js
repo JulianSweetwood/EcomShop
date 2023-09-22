6c86e9817403090236442ae97ea6170c78f494c2
JulianSweetwood <juliansussholz17@gmail.com>
2023-09-22 17:38:15 +0200

    errorMiddleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import Product from "../models/productModel.js";
 
 router.get('/', asyncHandler (async(req,res)=> {
     const products = await Product.find ({});
+ 
     res.json(products);
 }));
 
@@ -13,9 +14,10 @@ router.get('/:id', asyncHandler (async(req,res) => {
 
    if (product) {
     return res.json(product);
+       } else {
+        res.status(400);
+        throw new Error('Resource Not Found')
        }
-
-       res.status(404).json({ message: 'Product not found'});
 }));
 
 export default router
